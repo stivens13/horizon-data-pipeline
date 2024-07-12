d607547a83b697010be5039847e7ef2b93ef2670
Nikita Voloshenko <stivens54@gmail.com>
2024-07-11 17:58:35 -0700

    add ETL service

---
+++ services/etl/etl.go
@@ -0,0 +1,93 @@
+package etl
+
+import (
+	"fmt"
+	"github.com/gocarina/gocsv"
+	"github.com/stivens13/horizon-data-pipeline/models"
+	"github.com/stivens13/horizon-data-pipeline/services/currency_tracker"
+	"github.com/stivens13/horizon-data-pipeline/services/gcp"
+	"os"
+	"path"
+	"sort"
+)
+
+type ETL struct {
+	GCPStorage *gcp.GCPStorage
+	//CurrencyTracker *currency
+}
+
+var (
+	filename      = "sample_data.csv"
+	bucket        = ""
+	localDataDir  = "data/"
+	localFilepath = path.Join(localDataDir, filename)
+)
+
+const (
+	dateKeyLayout = "20060102"
+	EVENT_BUY     = "BUY_ITEMS"
+	EVENT_SELL    = "SELL_ITEMS"
+)
+
+func (e *ETL) ExtractTxs(filename string) error {
+	if err := e.GCPStorage.DownloadFile(bucket, filename, localDataDir+filename); err != nil {
+		return fmt.Errorf("error downloading data from GCS: %w", err)
+	}
+
+	return nil
+}
+
+func (e *ETL) ReadData(filepath string) (txs []*models.TransactionRaw, err error) {
+	txsFile, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return txs, fmt.Errorf("failed to open data file %v: %w", filename, err)
+	}
+	defer txsFile.Close()
+
+	if err := gocsv.UnmarshalFile(txsFile, &txs); err != nil { // Load txs from file
+		return txs, fmt.Errorf("failed to unmarshall csv file %v: %w", filename, err)
+	}
+
+	return txs, nil
+}
+
+func (e *ETL) TransformTxs() error {
+	txsRaw, err := e.ReadData(localFilepath)
+	if err != nil {
+		return fmt.Errorf("error reading transaction data: %w", err)
+	}
+	txsByDate := map[string][]*models.Transaction{}
+	for _, tx := range txsRaw {
+		date := tx.Timestamp.Format(dateKeyLayout)
+		txsByDate[date] = append(txsByDate[date], tx.ToTransaction())
+	}
+
+	keys := make([]string, 0, len(txsByDate))
+
+	for k := range txsByDate {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		txs := txsByDate[k]
+		totalVolume := e.CalculateDailyVolume(txs)
+		fmt.Printf("Date: %s, volume: %f\n", k, totalVolume)
+	}
+
+	return nil
+}
+
+func (e *ETL) CalculateDailyVolume(txs []*models.Transaction) float64 {
+	//tradesByCurrency := map[string]float64{}
+	totalVolume := 0.0
+	for _, tx := range txs {
+		totalVolume += tx.CurrencyValue * currency_tracker.GetCurrencyDailyValueInUSD(tx.CurrencySymbol)
+	}
+
+	return totalVolume
+}
+
+func (e *ETL) LoadTxs(date string) error {
+	return nil
+}
---
+++ services/etl/etl_test.go
@@ -0,0 +1,114 @@
+package etl
+
+import (
+	"github.com/stivens13/horizon-data-pipeline/models"
+	"github.com/stivens13/horizon-data-pipeline/services/gcp"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type ETLTestSuite struct {
+	suite.Suite
+}
+
+func (s *ETLTestSuite) SetupTest() {}
+
+func (s *ETLTestSuite) TearDownTest() {}
+
+func TestETLSuite(t *testing.T) {
+	suite.Run(t, new(ETLTestSuite))
+}
+
+//func (s *ETLTestSuite) TestExtractTxs() {
+//	type fields struct {
+//		GCPStorage *gcp.GCPStorage
+//	}
+//	type args struct {
+//		filename string
+//	}
+//	tests := []struct {
+//		name    string
+//		fields  fields
+//		args    args
+//		wantErr bool
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			e := &ETL{
+//				GCPStorage: tt.fields.GCPStorage,
+//			}
+//			if err := e.ExtractTxs(tt.args.filename); (err != nil) != tt.wantErr {
+//				t.Errorf("ExtractTxs() error = %v, wantErr %v", err, tt.wantErr)
+//			}
+//		})
+//	}
+//}
+
+func (s *ETLTestSuite) TestReadData() {
+	filepath := "data/sample_data.csv"
+	type fields struct {
+		GCPStorage *gcp.GCPStorage
+		filepath   string
+	}
+	tests := map[string]struct {
+		name     string
+		filepath string
+		fields   fields
+		wantTxs  []*models.TransactionRaw
+		wantErr  bool
+	}{
+		"sample data": {
+			fields:  fields{GCPStorage: &gcp.GCPStorage{}, filepath: filepath},
+			wantErr: false,
+		},
+	}
+	for name, test := range tests {
+		s.Run(name, func() {
+			e := &ETL{
+				GCPStorage: test.fields.GCPStorage,
+			}
+			gotTxs, err := e.ReadData(test.fields.filepath)
+			if test.wantErr {
+				s.Require().NoError(err)
+				return
+			}
+			s.Require().NoError(err)
+			s.Require().NotEmpty(gotTxs)
+		})
+	}
+}
+
+func (s *ETLTestSuite) TestTransformData() {
+	filepath := "data/sample_data.csv"
+	type fields struct {
+		GCPStorage *gcp.GCPStorage
+		filepath   string
+	}
+	tests := map[string]struct {
+		name     string
+		filepath string
+		fields   fields
+		wantTxs  []*models.TransactionRaw
+		wantErr  bool
+	}{
+		"sample data": {
+			fields:  fields{GCPStorage: &gcp.GCPStorage{}, filepath: filepath},
+			wantErr: false,
+		},
+	}
+	for name, test := range tests {
+		s.Run(name, func() {
+			e := &ETL{
+				GCPStorage: test.fields.GCPStorage,
+			}
+			err := e.TransformTxs()
+			if test.wantErr {
+				s.Require().NoError(err)
+				return
+			}
+			s.Require().NoError(err)
+		})
+	}
+}
